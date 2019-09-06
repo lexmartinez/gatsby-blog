75fd5670f8554b73a3e23390044dd825c0f24076
Lex Martinez <lex0316@gmail.com>
2019-09-06 15:53:50 -0500

    fixing footer links

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
+import { GitHub, Mail, Twitter } from 'react-feather'
+
 export default () => {
+  const theme = localStorage.getItem('theme');
+  const color = theme === 'dark' ? '#cdcdcd' : '#565656'
   return (
       <div className={'footer'}>
+        <div className={'footer-links'}>
+          <a href={'mailto:me@lexmartinez.com'} target={'_blank'}><Mail color={color} size={24}/></a>
+          <a href={'https://github.com/lexmartinez'} target={'_blank'}><GitHub color={color} size={24}/></a>
+          <a href={'https://twitter.com/lex0316'} target={'_blank'}><Twitter color={color} size={24}/></a>
+        </div>
         <p className={'footer-note'}>&copy; 2018-2019 <b>LEX MARTINEZ</b> All rights reserved.</p>
       </div>
   );
